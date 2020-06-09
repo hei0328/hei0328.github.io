8aa3d6405ecf198e41ef59ba429bf0ac5563adc0
aaron0328 <aaron0328@hotmail.com>
2020-06-10 04:38:13 +0800

    updated

---
+++ precache-manifest.5f260c2e48d1b6b69b0bb9eb97edde7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08e6d1f885f6ae48cf9b08415356d09d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56ed628748048782b32b",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ff338bb1966160c393b3",
+    "url": "/static/js/2.f31ba415.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f31ba415.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ed628748048782b32b",
+    "url": "/static/js/main.4586292b.chunk.js"
+  },
+  {
+    "revision": "5a9b688b036db462cbad",
+    "url": "/static/js/runtime-main.43713f6d.js"
+  }
+]);
